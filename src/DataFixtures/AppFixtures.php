42fbe441c77a48cabba8adaf91e3dfc85a26dfea
Schyzofrania <steve.crepin@gmail.com>
2021-02-03 15:16:07 +0100

    création database + login

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+                // Création d’un utilisateur de type “administrateur”
+        $admin = new Admin();
+        $admin->setEmail('admin@monsite.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordEncoder->encodePassword(
+        $admin,
+        'adminpassword'
+        ));
+        $admin->setPassword($this->passwordEncoder->encodePassword(
+            $admin,
+            'the_new_password'
+            ));
+        $manager->flush();
+    }
+}
